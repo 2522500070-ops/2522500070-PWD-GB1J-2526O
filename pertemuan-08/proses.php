b3a540abcd6ad85ff2e4b2f7fd7a5981392dd387
unknown <2522500070@mahasiswa.atmaluhur.ac.id>
2025-11-13 17:23:55 +0700

    buat codingan di file proses.php

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
-$_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
+$_SESSION["nim"] = $_POST["nim"];
+$_SESSION["nama"] = $_POST["nama"];
+$_SESSION["tempat"] = $_POST["tempat"];
+$_SESSION["tgl_lahir"] = $_POST["tgl_lahir"];
+$_SESSION["hobi"] = $_POST["hobi"];
+$_SESSION["pasangan"] = $_POST["pasangan"];
+$_SESSION["pekerjaan"] = $_POST["pekerjaan"];
+$_SESSION["ortu"] = $_POST["ortu"];
+$_SESSION["kakak"] = $_POST["kakak"];
+$_SESSION["adik"] = $_POST["adik"];
 header("location: index.php");
 ?>
