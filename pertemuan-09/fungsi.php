7b4c3a8982d48ce9ef08ba26c505ac46605fd515
unknown <2522500070@mahasiswa.atmaluhur.ac.id>
2025-12-02 20:01:49 +0700

    menambahkan fungsi <code>formatTanggal()

---
+++ pertemuan-09/fungsi.php
@@ -0,0 +1,15 @@
+<?php
+function bersihkan($str)
+{
+    return htmlspecialchars(trim($str));
+}
+
+function tidakKosong($str)
+{
+    return strlen(trim($str)) > 0;
+}
+
+function formatTanggal($tgl)
+{
+    return date("d M Y", strtotime($tgl));
+}
