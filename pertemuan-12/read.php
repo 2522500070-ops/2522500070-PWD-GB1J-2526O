518814516d6b4fee65477d955403ae99ffa76a43
unknown <2522500070@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:33:15 +0700

    Menambahkan kolom Aksi setelah kolom No dengan isi <td> Aksi sama dengan <td> ID pada read.php

---
+++ pertemuan-12/read.php
@@ -0,0 +1,30 @@
+<?php
+require 'koneksi.php';
+
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
+$no = 1;
+?>
+
+<table border="1" cellpadding="8" cellspacing="0">
+<tr>
+    <th>No</th>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Pesan</th>
+    <th>Created At</th>
+</tr>
+
+<?php while ($row = mysqli_fetch_assoc($q)) : ?>
+<tr>
+    <td><?= $no++; ?></td>
+    <td><?= $row['cid']; ?></td>
+    <td><?= htmlspecialchars($row['cnama']); ?></td>
+    <td><?= htmlspecialchars($row['cemail']); ?></td>
+    <td><?= htmlspecialchars($row['cpesan']); ?></td>
+    <td><?= date('d-m-Y H:i:s', strtotime($row['created_at'])); ?></td>
+</tr>
+<?php endwhile; ?>
+
+</table>
